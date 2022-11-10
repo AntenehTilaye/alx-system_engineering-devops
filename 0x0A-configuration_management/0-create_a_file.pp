c0c85872e61d2e9b097a3e759b69bfb96069b785
Anteneh Tilaye <antenehtilaye19@gmail.com>
2022-11-10 17:21:50 +0300

    configuration_management done

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+#Create school file in /tmp
+
+file {'/tmp/school':
+  ensure  => file,
+  path    => '/tmp/school',
+  content => 'I love Puppet',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+}
